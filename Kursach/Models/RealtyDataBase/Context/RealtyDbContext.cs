40256521e313e4980fc154de525c58f2aae0134d
Michael <83959582+Yaspeace@users.noreply.github.com>
2022-02-01 01:37:26 +0300

    Добавьте файлы проекта.

---
+++ Kursach/Models/RealtyDataBase/Context/RealtyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kursach.Models.RealtyDataBase.Context
+{
+    public class RealtyDbContext : DbContext
+    {
+        /*DbSet<...*/
+
+        public RealtyDbContext(DbContextOptions opt) : base(opt)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}

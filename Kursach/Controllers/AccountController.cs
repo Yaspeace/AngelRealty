59e5c1c461beacf7d6711f8dfed1ba58a7c5e892
Michael <83959582+Yaspeace@users.noreply.github.com>
2022-02-04 15:39:15 +0300

    Добавил возможность выхода и регистрации

--- Kursach/Controllers/AccountController.cs
+++ Kursach/Controllers/AccountController.cs
@@ -27,19 +27,61 @@ namespace Kursach.Controllers
         }
 
         [HttpPost]
-        public async Task<RedirectResult> Login(string email, string password)
+        public async Task<IActionResult> Login(string email, string password)
         {
             if (db.users.Any(usr => usr.email == email && usr.password == password))
             {
-                var claims = new List<Claim> { new Claim(ClaimsIdentity.DefaultNameClaimType, email) };
-                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme, ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                return Redirect("/Home/Index");
+                await Authenticate(email);
+                return RedirectToAction("Index", "Home");
             }
             else
             {
-                return Redirect("Login");
+                return RedirectToAction("Login", "Account");
             }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserModel user)
+        {
+            if (user != null && user.name != null && user.name != ""
+                && user.surname != null && user.surname != ""
+                && user.phone != null && user.phone != ""
+                && user.email != null && user.email != ""
+                && user.password != null && user.password != "")
+            {
+                if (!db.users.Any(u => u.email == user.email))
+                {
+                    user.id = db.users.Max(u => u.id) + 1;
+                    db.users.Add(user);
+                    db.SaveChanges();
+                    await Authenticate(user.email);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                    return RedirectToAction("Register", "Account");
+            }
+            else
+                return RedirectToAction("Register", "Account");
+
+        }
+
+        public async Task Authenticate(string email)
+        {
+            var claims = new List<Claim> { new Claim(ClaimsIdentity.DefaultNameClaimType, email) };
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme, ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
         }
     }
 }
